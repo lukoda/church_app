9e83d493433b6a0adc22120033eb7ba33ecfb311
lukoda <lukoda@github.com>
2025-09-15 07:39:34 +0300

    fix button on create new church member

--- resources/views/filament/pages/create-new-church-member.blade.php
+++ resources/views/filament/pages/create-new-church-member.blade.php
@@ -3,11 +3,6 @@
             <form wire:submit="createChurchMember">
                 {{ $this->form }}
 
-                <x-filament::button
-                    type="submit"
-                    wire:target="createChurchMember">
-                    Create New Church Member
-                </x-filament::button>                
             </form>
             
             <x-filament-actions::modals />
